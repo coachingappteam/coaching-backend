ff2f1e1ae87e8271b02ce38588c1513feaedc5ef
Alberto De Jesus <alberto.dejesus@upr.edu>
2019-10-19 18:22:11 -0400

    Added credentials for Heroku DB

--- src/pg_config.py
+++ src/pg_config.py
@@ -1,17 +1,15 @@
 #This file contains the data base configuration information
 
 
-"""
 pg_config = {
-    'user':'tgychgsgfepvcn',
-    'passwd': 'd4514c4ec11b49119c1462cbb17b8067fad267d6119bfdb7f0d52121f5ce65f4',
-    'dbname': 'd6b8l0k78qus2u',
-    'host': 'ec2-23-23-222-184.compute-1.amazonaws.com',
+    'user':'vuxrvlshpxdoza',
+    'passwd': '667bf971049b8ec4697c7061b6ef28df225c097c37d59f90316d609022decf80',
+    'dbname': 'd54r2ffdijl58s',
+    'host': 'ec2-54-221-217-204.compute-1.amazonaws.com',
     'port': '5432'
 }
+
 """
-
-
 pg_config = {
     'user':'coaching_admin',
     'passwd': 'admin',
@@ -19,3 +17,4 @@ pg_config = {
     'host': '127.0.0.1',
     'port': 5432
 }
+"""
